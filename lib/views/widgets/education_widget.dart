794bbfbfebbbb122ce5b3915466e8e4dc510581f
Priyank <priyankprmr@gmail.com>
2025-09-11 17:53:41 +0530

    - created education widget

---
+++ lib/views/widgets/education_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/common/gap.dart';
+import 'package:portfolio/widgets/work_experience_card.dart';
+
+class EducationWidget extends StatelessWidget {
+  const EducationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'EDUCATION',
+          style: TextStyle(
+            color: Colors.teal.shade700,
+            fontWeight: FontWeight.w600,
+            fontSize: 16.0,
+          ),
+        ),
+        Gap(height: 8.0),
+        WorkExperienceCard(),
+      ],
+    );
+  }
+}

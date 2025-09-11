d5c78ca12ee02c929038d6fd7fee68e1bb805fe0
Priyank <priyankprmr@gmail.com>
2025-09-11 16:10:10 +0530

    - work experience card widget changes

--- lib/widgets/work_experience_card.dart
+++ lib/widgets/work_experience_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/common/horizontal_dot_spacer.dart';
+import 'package:portfolio/common/bullet_point_text.dart';
 import 'package:portfolio/utils/extensions.dart';
 
 class WorkExperienceCard extends StatelessWidget {
@@ -12,55 +12,26 @@ class WorkExperienceCard extends StatelessWidget {
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Text('Flutter Developer'), Text('08/24 - 06/25')],
+          children: [
+            Text(
+              'Flutter Developer',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('08/24 - 06/25'),
+          ],
         ),
         Text('Helmsman Consultancy'),
         Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                HorizontalDotSpacer().verticalPadding(6.0),
-                Flexible(
-                  child: Text(
-                    'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
-                    // textDirection: TextDirection.ltr,
-                    // textAlign: TextAlign.left,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                HorizontalDotSpacer().verticalPadding(6.0),
-                Flexible(
-                  child: Text(
-                    'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
-                    // textDirection: TextDirection.ltr,
-                    // textAlign: TextAlign.left,
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                HorizontalDotSpacer().verticalPadding(6.0),
-                Flexible(
-                  child: Text(
-                    'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
-                    // textDirection: TextDirection.ltr,
-                    // textAlign: TextAlign.left,
-                  ),
-                ),
-              ],
+            BulletPointText(
+              text:
+                  'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum',
             ),
           ],
         ),
       ],
-    );
+    ).verticalPadding(8.0);
   }
 }

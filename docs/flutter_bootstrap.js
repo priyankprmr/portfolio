107b7619d40a79fe00f1dd995f22fe6912d6b3a6
Priyank <priyankprmr@gmail.com>
2025-09-17 16:50:24 +0530

    Update web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3012210341"
+    serviceWorkerVersion: "2720862254"
   }
 });

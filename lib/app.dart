87b3ca1d8ba890d6d7d74c887fd447990ab4a548
Priyank <priyankprmr@gmail.com>
2025-09-19 12:10:51 +0530

    - cursor performance improvements

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:portfolio/core/extensions/add_mouse_region_extension.dart';
 import 'package:portfolio/core/providers/cursor_provider.dart';
 import 'package:portfolio/core/utils/const_texts.dart';
+import 'package:portfolio/core/widgets/cursor_overlay.dart';
 import 'package:portfolio/core/widgets/top_navbar.dart';
 import 'package:portfolio/features/home/home_page.dart';
 import 'package:portfolio/features/projects/projects_page.dart';
@@ -19,59 +20,16 @@ class MainApp extends StatelessWidget {
       routes: <RouteBase>[
         ShellRoute(
           builder: (context, state, child) {
-            return Consumer<CursorProvider>(
-              builder: (context, cursor, _) {
-                return Stack(
-                  children: [
-                    // Trailing dots
-                    ...cursor.dots.map(
-                      (dot) => Positioned(
-                        left: dot.position.dx - 15,
-                        top: dot.position.dy - 15,
-                        child: Opacity(
-                          opacity: dot.opacity,
-                          child: Container(
-                            width: cursor.cursorSize.width,
-                            height: cursor.cursorSize.height,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: Colors.teal[700]!,
-                                width: 2,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    // Ring cursor
-                    Positioned(
-                      left: cursor.cursorCenter.$1,
-                      top: cursor.cursorCenter.$2,
-                      child: Container(
-                        width: cursor.cursorSize.width,
-                        height: cursor.cursorSize.height,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.teal[700]!,
-                            width: 2,
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    
-                    Scaffold(
-                      appBar: TopNavbar(),
-                      backgroundColor: Colors.transparent,
-                      body: child,
-                    ),
-                  ],
-                ).withMouseRegion(context.read<CursorProvider>().onMouseMove);
-              },
-            );
+            return Stack(
+              children: [
+                Scaffold(
+                  appBar: TopNavbar(),
+                  backgroundColor: Colors.transparent,
+                  body: child,
+                ),
+                CursorOverlay(),
+              ],
+            ).withMouseRegion(context.read<CursorProvider>().onMouseMove);
           },
           routes: [
             GoRoute(
---
+++ lib/core/widgets/cursor_overlay.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/core/providers/cursor_provider.dart';
+import 'package:provider/provider.dart';
+
+class CursorOverlay extends StatelessWidget {
+  const CursorOverlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      child: Consumer<CursorProvider>(
+        builder: (context, cp, child) {
+          return Stack(
+            children: [
+              // Trailing dots
+              ...cp.dots.map(
+                (dot) => Positioned(
+                  left: dot.position.dx - 15,
+                  top: dot.position.dy - 15,
+                  child: Opacity(
+                    opacity: dot.opacity,
+                    child: Container(
+                      width: cp.cursorSize.width,
+                      height: cp.cursorSize.height,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.teal.shade700,
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              // Ring cp
+              Positioned(
+                left: cp.cursorCenter.$1,
+                top: cp.cursorCenter.$2,
+                child: Container(
+                  width: cp.cursorSize.width,
+                  height: cp.cursorSize.height,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.teal.shade700, width: 2),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
